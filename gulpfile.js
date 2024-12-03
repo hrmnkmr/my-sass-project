545306542ed3b439350fd5d42feff7a428c26b28
nakamura <hrm.nkmr04@gmail.com>
2024-12-04 07:50:31 +0900

    second commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-// Sass のコンパイルタスク
-function compileSass() {
-  return gulp
-    .src("sass/style.scss") // Sass ファイルの場所
-    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
-    .pipe(gulp.dest("css")); // CSS の出力先
-}
+// Sassコンパイルタスク
+gulp.task('sass', function() {
+  return gulp.src('src/scss/**/*.scss') // SCSSファイルのパスを指定
+    .pipe(sass().on('error', sass.logError)) // Sassをコンパイル
+    .pipe(gulp.dest('dist/css')); // 出力先のパス
+});
 
-// Sass ファイルを監視するタスク
-function watchSass() {
-  gulp.watch("sass/style.scss", compileSass);
-}
-
-// デフォルトタスク
-exports.default = watchSass;
+// watchタスク（SCSSファイルが変更されたら自動でコンパイル）
+gulp.task('watch', function() {
+  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+});
