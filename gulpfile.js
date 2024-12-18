7d1e8f917f56d29a8578a5e0632d59dfa67d41bb
nakamura <hrm.nkmr04@gmail.com>
2024-12-19 07:26:35 +0900

    fourth commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps'); // ソースマップ対応
 
-// Sassコンパイルタスク
-gulp.task('sass', function() {
-  return gulp.src('src/scss/**/*.scss') // SCSSファイルのパスを指定
-    .pipe(sass().on('error', sass.logError)) // Sassをコンパイル
-    .pipe(gulp.dest('dist/css')); // 出力先のパス
+gulp.task('sass', function () {
+    return gulp.src('src/scss/style.scss') // 入力元
+        .pipe(sourcemaps.init()) // ソースマップ初期化
+        .pipe(sass().on('error', sass.logError)) // Sassコンパイル
+        .pipe(sourcemaps.write('.')) // ソースマップを同じディレクトリに出力
+        .pipe(gulp.dest('src/css')); // 出力先をsrc/cssに変更
 });
 
-// watchタスク（SCSSファイルが変更されたら自動でコンパイル）
-gulp.task('watch', function() {
-  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+gulp.task('watch', function () {
+    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
 });
 
-// defaultタスクを定義（watchを実行するよう設定）
 gulp.task('default', gulp.series('sass', 'watch'));
