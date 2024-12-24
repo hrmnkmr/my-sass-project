54a94cae29fa142a5c164f78b49fb71255aff3e4
nakamura <hrm.nkmr04@gmail.com>
2024-12-25 08:23:42 +0900

    fifth commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps'); // ソースマップ対応
 
 gulp.task('sass', function () {
-    return gulp.src('src/scss/style.scss') // 入力元
-        .pipe(sourcemaps.init()) // ソースマップ初期化
-        .pipe(sass().on('error', sass.logError)) // Sassコンパイル
-        .pipe(sourcemaps.write('.')) // ソースマップを同じディレクトリに出力
-        .pipe(gulp.dest('src/css')); // 出力先をsrc/cssに変更
+  return gulp.src('src/scss/style.scss') // 入力元
+    .pipe(sourcemaps.init()) // ソースマップ初期化
+    .pipe(sass().on('error', sass.logError)) // Sassコンパイル
+    .pipe(sourcemaps.write('.')) // ソースマップを同じディレクトリに出力
+    .pipe(gulp.dest('src/css')); // 出力先をsrc/cssに変更
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
 });
 
 gulp.task('default', gulp.series('sass', 'watch'));
